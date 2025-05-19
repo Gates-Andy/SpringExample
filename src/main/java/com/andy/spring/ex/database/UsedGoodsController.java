9ec44cfd597e054fc2ee93efe20217f6ec037a27
andy <andy.com>
2025-05-19 18:51:59 +0900

    [신윤식] -미완성 내일

---
+++ src/main/java/com/andy/spring/ex/database/UsedGoodsController.java
@@ -0,0 +1,13 @@
+package com.andy.spring.ex.database;
+
+import org.springframework.stereotype.Controller;
+
+// request, response 처리 기능
+@Controller
+public class UsedGoodsController {
+	// 중고 게시글 모든 정보를 response에 담는 페이지
+	@RequestMapping("/db/usedgoods/list")
+	public usedGoodsList() {
+		// 중고 게시글 정보 얻어 오기
+	}
+}
---
+++ src/main/java/com/andy/spring/ex/database/service/UsedGoodsService.java
@@ -0,0 +1,11 @@
+package com.andy.spring.ex.database.service;
+
+import org.springframework.stereotype.Service;
+
+@Service // 로직(busisness) 담당 (데이터 가공 햄버거 조립) 그 외 복잡한 기능 수행 코드가 제일 더러움
+public class UsedGoodsService {
+	// 중고 게시글 정보 모두 얻어오기
+	public getUsedGoodsList(){
+		
+	}
+}
