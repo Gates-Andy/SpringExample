ba86f0a4f6361c026f93e9606dc8d6fc32a986ae
andy <andy.com>
2025-06-10 18:48:27 +0900

    [신윤식] -컨트롤러 boolean return false 그리고 true

--- src/main/java/com/andy/spring/ex/mvc/repository/UserRepository.java
+++ src/main/java/com/andy/spring/ex/mvc/repository/UserRepository.java
@@ -1,14 +1,18 @@
 package com.andy.spring.ex.mvc.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.andy.spring.ex.mvc.domain.User;
 
 @Mapper
 public interface UserRepository {
-	//1.
+	// 1.
 	public User selectLastUser();
-	
-	//2.
-	public int insertUser(User user); 
+
+	// 2.
+	public int insertUser(User user);
+
+	// 3. Ajax 전달 받은 email이 포함된 행의 개수 조회
+	public int selectCountByEmail(@Param("email") String email);
 }
--- src/main/java/com/andy/spring/ex/mvc/service/UserService.java
+++ src/main/java/com/andy/spring/ex/mvc/service/UserService.java
@@ -10,21 +10,35 @@ import com.andy.spring.ex.mvc.repository.UserRepository;
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-	
-	//1. 가장 최근에 등록한 사용자 정보 얻어오기 repository로 request
-	public User getLastUser(){
-		
+
+	// 1. 가장 최근에 등록한 사용자 정보 얻어오기 repository로 request
+	public User getLastUser() {
+
 		User user = userRepository.selectLastUser();
-		
+
 		return user;
 	}
-	//2.
+
+	// 2.
 	public int addUser(User user) {
-		
+
 		int count = userRepository.insertUser(user);
-		
+
 		return count;
-		
+
 	}
-	
+
+	// AJAX 전달받은 email이 중복된 것인지 판별
+	public boolean isDuplicateEmail(String email) {
+
+		int count = userRepository.selectCountByEmail(email); //리포지터리에서 그 이메일이 검색이되면 1이 뜰거고 0이면 없다는 뜻일것
+		
+		if (count == 0) { // 0이라는 count는 이메일이 사용가능하다는 뜻
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
 }
