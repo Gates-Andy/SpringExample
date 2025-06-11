4020466fcfc1b45c3ade5c66e056bf50eadbe94c
Andy <Andy.com>
2025-06-11 21:29:29 +0900

    [신윤식] -20250610 컨트롤러

--- src/main/java/com/andy/spring/ex/ajax/NewUserController.java
+++ src/main/java/com/andy/spring/ex/ajax/NewUserController.java
@@ -78,5 +78,7 @@ public class NewUserController {
 
 		}
 		return resultMap;
+		
 	}
+	
 }
--- src/main/java/com/andy/spring/ex/mvc/service/UserService.java
+++ src/main/java/com/andy/spring/ex/mvc/service/UserService.java
@@ -31,14 +31,22 @@ public class UserService {
 	// AJAX 전달받은 email이 중복된 것인지 판별
 	public boolean isDuplicateEmail(String email) {
 
-		int count = userRepository.selectCountByEmail(email); //리포지터리에서 그 이메일이 검색이되면 1이 뜰거고 0이면 없다는 뜻일것
-		
-		if (count == 0) { // 0이라는 count는 이메일이 사용가능하다는 뜻
-			return false;
-		} else {
-			return true;
-		}
+		int count = userRepository.selectCountByEmail(email); // 리포지터리에서 그 이메일이 검색이되면 1이 뜰거고 0이면 없다는 뜻일것
 
+		if (count == 0) { // 0이라는 count는 이메일이 사용가능하다는 뜻
+
+			return false; // true가 중복 있다는 뜻이고, false가 중복 없다는 뜻
+
+		} else {
+
+			return true;
+
+		}
+// 이메일이 중복되었으면 true 반환, 중복 안되었으면 false 반환
+//		public boolean isDuplicateEmail(String email) {
+//		    int count = userRepository.selectCountByEmail(email);
+//		    return count > 0;
+//		}
 	}
 
 }
